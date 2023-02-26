8d3621907a7f03d52b89eb13dc24a799a86d8ecc
lTimej <lTimej@126.com>
2023-02-26 16:34:35 +0800

    初始化

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Time_go-zero_csdn
 this is a go-zero exercise
+# 快速生成rpc服务
+goctl rpc new 服务名
+# 快速生成api
+goctl api new api
+# 通过api文件生成go文件
+goctl api go -api core.api -dir . -style go_zero
+# 通过proto生成go文件
+goctl rpc protoc user.proto --go_out=. --go-grpc_out=. --zrpc_out=.
---
+++ csdn/user/api/internal/handler/apihandler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"liujun/Time_go-zero_csdn/csdn/user/api/internal/logic"
+	"liujun/Time_go-zero_csdn/csdn/user/api/internal/svc"
+	"liujun/Time_go-zero_csdn/csdn/user/api/internal/types"
+)
+
+func ApiHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.Request
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := logic.NewApiLogic(r.Context(), svcCtx)
+		resp, err := l.Api(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
