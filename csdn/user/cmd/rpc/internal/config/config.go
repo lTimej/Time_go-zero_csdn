d1d794f0bceda717c03ce5aebc08c9945d3599bf
lTimej <lTimej@126.com>
2023-02-26 19:02:14 +0800

    初始化mysql、redis

--- csdn/user/cmd/rpc/internal/config/config.go
+++ csdn/user/cmd/rpc/internal/config/config.go
@@ -4,4 +4,12 @@ import "github.com/zeromicro/go-zero/zrpc"
 
 type Config struct {
 	zrpc.RpcServerConf
+	Mysql struct {
+		DataSource string
+	}
+	Redis struct {
+		Host     string
+		Password string
+		DB       int
+	}
 }
--- csdn/user/cmd/rpc/internal/svc/servicecontext.go
+++ csdn/user/cmd/rpc/internal/svc/servicecontext.go
@@ -1,13 +1,22 @@
 package svc
 
-import "liujun/Time_go-zero_csdn/csdn/user/cmd/rpc/internal/config"
+import (
+	"github.com/go-redis/redis/v8"
+	"gorm.io/gorm"
+	"liujun/Time_go-zero_csdn/csdn/user/cmd/rpc/internal/config"
+	"liujun/Time_go-zero_csdn/csdn/user/model"
+)
 
 type ServiceContext struct {
-	Config config.Config
+	Config    config.Config
+	UserMysql *gorm.DB
+	UserRedis *redis.Client
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:    c,
+		UserMysql: model.InitMysql(c.Mysql.DataSource),
+		UserRedis: model.InitRedis(c.Redis.Host, c.Redis.Password, c.Redis.DB),
 	}
 }
